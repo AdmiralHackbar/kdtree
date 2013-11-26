ba33e83b707eaf82c10a9dc10a39c7c0dc3c61a8
AdmiralHackbar <the.admiral.hackbar@gmail.com>
2013-11-25 21:22:19 -0600

    Adding basic unit test.

---
+++ src/test/java/com/admiralhackbar/kdtree/TestKDTree.java
@@ -0,0 +1,45 @@
+package com.admiralhackbar.kdtree;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
+import junit.framework.Assert;
+import org.junit.Test;
+
+import javax.annotation.Nullable;
+import java.awt.*;
+import java.util.List;
+
+/**
+ */
+public class TestKDTree {
+
+    @Test
+    public void testFind() {
+        List<Point> points = Lists.newArrayList();
+        points.add(new Point(1,1));
+        points.add(new Point(2,2));
+        points.add(new Point(3,3));
+        points.add(new Point(4,4));
+        points.add(new Point(5,5));
+        List<Function<Point, Float>> functions = Lists.newArrayList();
+        functions.add(new Function<Point, Float>() {
+            @Nullable
+            @Override
+            public Float apply(@Nullable Point input) {
+                return (float)input.getX();
+            }
+        });
+        functions.add(new Function<Point, Float>() {
+            @Nullable
+            @Override
+            public Float apply(@Nullable Point input) {
+                return (float)input.getY();
+            }
+        });
+
+        final KDTree<Point> kdTree = (new KDTreeBuilder<Point>(functions)).addAll(points).build();
+        Assert.assertEquals(new Point(3,3), kdTree.find(new Point(3,3)));
+        Assert.assertNull(kdTree.find(new Point(0,0)));
+
+    }
+}
